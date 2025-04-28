f53b1b810c49a79079ea11bcb3e26f683f20b111
CanerTanriverdi <canertanriverdiis@gmail.com>
2025-04-28 16:10:16 +0300

    product category completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import { useAppContext } from "./context/AppContext";
 import Login from "./components/Login";
 import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -27,6 +28,10 @@ const App = () => {
           <Route
             path="/products"
             element={<AllProducts />}
+          />
+          <Route
+            path="/products/:category"
+            element={<ProductCategory />}
           />
         </Routes>
       </div>
